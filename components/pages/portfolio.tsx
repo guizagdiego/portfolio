d5de1e1c1f0b149f5ef671b96f0ce0a5a00eeeea
O2F-IT\diegofernando.guiza_ <diegofernando.guiza@amaris.com>
2026-02-25 10:08:39 -0500

    feat: add portfolio development page.

---
+++ components/pages/portfolio.tsx
@@ -0,0 +1,9 @@
+const PortfolioPage = () => {
+  return (
+    <>
+      <div className='no-scrollbar portfolio-container relative size-full snap-y snap-mandatory overflow-y-scroll'></div>
+    </>
+  );
+};
+
+export default PortfolioPage;
