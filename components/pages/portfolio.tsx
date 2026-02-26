21dc671387813e4f7b8df0c4d84fc77d60df59b3
O2F-IT\diegofernando.guiza_ <diegofernando.guiza@amaris.com>
2026-02-26 09:09:26 -0500

    feat: add navbar with dark mode support and zustand state.

--- components/pages/portfolio.tsx
+++ components/pages/portfolio.tsx
@@ -1,7 +1,10 @@
+import Navbar from './sections/navbar';
+
 const PortfolioPage = () => {
   return (
     <>
       <div className='no-scrollbar portfolio-container relative size-full snap-y snap-mandatory overflow-y-scroll'></div>
+      <Navbar />
     </>
   );
 };
---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  title: 'Siddharth Sharma',
+  description: 'my personal portfolio website showcasing projects, skills, and other cool stuffs',
+  url: '', //env.NEXT_PUBLIC_APP_URL,
+  twitter: 'https://x.com/404',
+  linkedin: 'https://linkedin.com/in/diego-fernando-güiza-galeano-692051126',
+  github: `https://github.com/guizagdiego`,
+  email: 'diego.guizag@gmail.com',
+  whatsApp: 'https://wa.me/573197350144?text=Psst...  ¿Te gustaría charlar personalmente?',
+};
